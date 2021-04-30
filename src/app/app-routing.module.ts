da0f91e323f7cf5fd3899ca9485e3b3db7fe77f1
Ondrej Peterka <ondrej.peterka@iridium-technology.com>
2021-04-30 14:23:17 +0200

    Fix: scroll to top of the page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: 'list', component: ArticleListPageComponent, data:{'title': "Seznam článků"} },];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
