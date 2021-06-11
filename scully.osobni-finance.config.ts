7ba68a1296ca519ffe6563f9a25b251aed326dfc
Ondrej Peterka <ondrej.peterka@iridium-technology.com>
2021-06-11 18:56:03 +0200

    trying the disableAngular

--- scully.osobni-finance.config.ts
+++ scully.osobni-finance.config.ts
@@ -1,11 +1,33 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 require('@notiz/scully-plugin-lazy-images');
+const {DisableAngular} = require('scully-plugin-disable-angular');
+//import { getDelayAngularPlugin } from '@flowaccount/scully-plugin-angular-delay';
+
+
+setPluginConfig(DisableAngular, 'postProcessByHtml', {
+  ignoreRoutes: [
+    '/financni-kalkulacky', 
+  ]
+});
+
+const postRenderers = [
+  DisableAngular,
+  'lazyImages',
+  // getDelayAngularPlugin({
+  //   routesBlacklist: [
+  //     { route: '/financni-kalkulacky', removeAngular: false },
+  //     // { route: '/functions', removeAngular: true }
+  //   ],
+  //   delayMilliseconds: 1500
+  // })
+];
+
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "osobni-finance",
   outDir: './static-ready',
-  defaultPostRenderers: ['lazyImages'],
+  defaultPostRenderers: postRenderers,
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
@@ -15,3 +37,5 @@ export const config: ScullyConfig = {
     },
   }
 };
+
+
